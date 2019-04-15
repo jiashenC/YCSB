a23bb98b55fdb494873dcbd009b4e9448bbd0720
Jiashen Cao <caojiashen24@gmail.com>
2019-04-14 22:25:52 -0400

    skelecton client

--- blizzard/src/main/java/com/yahoo/ycsb/db/BlizzardClient.java
+++ blizzard/src/main/java/com/yahoo/ycsb/db/BlizzardClient.java
@@ -1,4 +1,50 @@
 package com.yahoo.ycsb.db;
 
-public class BlizzardClient {
+import com.yahoo.ycsb.ByteIterator;
+import com.yahoo.ycsb.DB;
+import com.yahoo.ycsb.DBException;
+import com.yahoo.ycsb.Status;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+public class BlizzardClient extends DB {
+  @Override
+  public void init() throws DBException {
+    // do nothing for now
+  }
+
+  @Override
+  public Status read(final String table, final String key, final Set<String> fields,
+                     final Map<String, ByteIterator> result) {
+    // do nothing for now
+    return Status.OK;
+  }
+
+  @Override
+  public Status scan(final String table, final String startkey, final int recordcount, final Set<String> fields,
+                     final Vector<HashMap<String, ByteIterator>> result) {
+    // do nothing for now
+    return Status.OK;
+  }
+
+  @Override
+  public Status update(final String table, final String key, final Map<String, ByteIterator> values) {
+    // do nothing for now
+    return Status.OK;
+  }
+
+  @Override
+  public Status insert(final String table, final String key, final Map<String, ByteIterator> values) {
+    // do nothing for now
+    return Status.OK;
+  }
+
+  @Override
+  public Status delete(final String table, final String key) {
+    // do nothing for now
+    return Status.OK;
+  }
 }
